e81c5cb9301bb33c1313f0d85ae653ea0f9a343a
walidpfe <walid.pfe@gmail.com>
2012-03-30 12:51:34 +0100

    company note afichage
    company note afichage

--- models/company.py
+++ models/company.py
@@ -4,20 +4,16 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 from google.appengine.api import mail
-
+from models.userprofilemodel import UserProfileModel
 
 class CompanyModel(db.Model):
      companyaddedby     = db.UserProperty(required=True)
      companyname        = db.StringProperty()
-     companytel         = db.StringProperty()
-     companymail        = db.StringProperty()
      companywebsite     = db.StringProperty()
      companyaddress     = db.StringProperty(multiline=True)
      companywilaya      = db.StringProperty()
-     companydescription = db.StringProperty(multiline=True)
+     companydescription = db.TextProperty()
      companydateadded   = db.DateTimeProperty(auto_now_add=True)
-
-
      @classmethod
      def getCompanyByName(self,name):
           if CompanyModel.all().filter('companyname = ',name).get():
@@ -26,10 +22,16 @@ class CompanyModel(db.Model):
                ref = CompanyModel( companyaddedby  = users.get_current_user(),
                                    companyname = name).put()
           return ref
-          
 
-
-
+class CompanyTelModel(db.Model):
+     companytel    = db.StringProperty()  
+     company       = db.ReferenceProperty(CompanyModel,
+      collection_name = 'tel')
+     @classmethod
+     def getAllTelsByCompanyID(self,id):
+      return CompanyTelModel.all().filter('company =',
+        CompanyModel.get_by_id(id))    
+    
 class CompanyEmailsModel(db.Model):
   email    = db.StringProperty()  
   company       = db.ReferenceProperty(CompanyModel,
@@ -64,3 +66,11 @@ class CompanyAddressesesModel(db.Model):
   def id_or_name(self):
     return self.key().id_or_name()
 
+
+class NoteCompanyModel(db.Model):
+     profile     = db.ReferenceProperty(UserProfileModel,
+     collection_name = 'entreprisenotepar')
+     creedate   = db.DateTimeProperty(auto_now_add=True)
+     texnote = db.TextProperty()
+     company     = db.ReferenceProperty(CompanyModel,
+     collection_name = 'cmpanynotepar')
